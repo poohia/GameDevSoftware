0395c7d174a581498de139bc8a7b540fa9ea9ba1
Jordan Azoulay <azoulay.jordan@gmail.com>
2022-04-30 18:26:00 +0200

    append options at useTabs

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,10 +1,30 @@
 import { useTabs } from 'renderer/hooks';
 import { Tab } from 'semantic-ui-react';
 import useApp from 'renderer/useApp';
+import { useMemo } from 'react';
+import {
+  AssetPage,
+  ConstantPage,
+  HomePage,
+  TranslationPage,
+} from 'renderer/pages';
+import { UseTabsProps } from 'renderer/hooks/useTabs';
 
 export default function App() {
   const { path } = useApp();
-  const { tabs, tabActive, onTabChange } = useTabs();
+  const tabOptions: UseTabsProps = useMemo(() => {
+    const modules: any = [];
+    modules['HomePage'] = HomePage;
+    modules['TranslationPage'] = TranslationPage;
+    modules['ConstantPage'] = ConstantPage;
+    modules['AssetPage'] = AssetPage;
+    return {
+      modules,
+      tableTabs: 'tabs',
+      tableActiveTab: 'tab-active',
+    };
+  }, []);
+  const { tabs, tabActive, onTabChange } = useTabs(tabOptions);
 
   if (path === undefined) {
     return <div>Loading....</div>;
