745b1889f3bbac28b469786cbbac80edbb5b3e2a
joazco <jazoulay@joazco.com>
2023-01-29 17:26:28 +0100

    avancement

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,7 +1,8 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
-import 'semantic-ui-css/semantic.min.css';
+// import 'semantic-ui-css/semantic.min.css';
+// import '../../assets/semantic.min.css';
 import 'renderer/index.scss';
 
 const container = document.getElementById('root')!;
