96722156cc354c5c25456adc3ee40711bb3d7ef4
Jordan AZOULAY <jazoulay@joazco.com>
2025-02-07 16:09:54 +0100

    feature: avancement

--- src/renderer/pages/ScenePage/components/SceneContainerComponent/useSceneContainerComponent.tsx
+++ src/renderer/pages/ScenePage/components/SceneContainerComponent/useSceneContainerComponent.tsx
@@ -92,14 +92,17 @@ const useSceneContainerComponent = (props: PageProps) => {
         _font: {
           label: 'Primary Font',
           core: 'font',
+          optional: true,
         },
         _music: {
           label: 'Primary music',
           core: 'sound',
+          optional: true,
         },
         _background: {
           label: 'Background of scene',
           core: 'image',
+          optional: true,
         },
       };
 
